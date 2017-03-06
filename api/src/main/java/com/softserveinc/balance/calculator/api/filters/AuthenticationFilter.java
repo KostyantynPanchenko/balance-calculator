c227221a2eed8238574a82e78191435b8fc03c83
KostyantynPanchenko <kostyantyn.panchenko@gmail.com>
2017-03-06 22:49:58 +0200

    Changed header

--- api/src/main/java/com/softserveinc/balance/calculator/api/filters/AuthenticationFilter.java
+++ api/src/main/java/com/softserveinc/balance/calculator/api/filters/AuthenticationFilter.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.ext.Provider;
 
 @Provider
@@ -12,7 +13,7 @@ public class AuthenticationFilter implements ContainerRequestFilter {
     @Override
     public void filter(ContainerRequestContext requestContext)
             throws IOException {
-        String authorization = requestContext.getHeaderString("Authorization");
+        String authorization = requestContext.getHeaderString(HttpHeaders.AUTHORIZATION);
         System.out.println(authorization);
     }
 
