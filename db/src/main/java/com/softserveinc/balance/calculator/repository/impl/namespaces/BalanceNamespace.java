335221b49757199b5431c62bff9c4e5b906311af
KostyantynPanchenko <kostyantyn.panchenko@gmail.com>
2017-03-11 15:05:21 +0200

    Fixed balance namespace

--- db/src/main/java/com/softserveinc/balance/calculator/repository/impl/namespaces/BalanceNamespace.java
+++ db/src/main/java/com/softserveinc/balance/calculator/repository/impl/namespaces/BalanceNamespace.java
@@ -14,8 +14,8 @@ public class BalanceNamespace {
     public final static String CREATED_BY_COLUMN_NAME = "created_by";
     public final static String TOTAL_ALLOCATED_CONSUMPTION_AMOUNT = "total_allocated_contribution_amount";
     public final static String TOTAL_ALLOCATED_CONTRIBUTION_AMOUNT = "total_allocated_consumption_amount";
-    public final static String TOTAL_UNALLOCATED_CONSUMPTION_AMOUNT = "total_unallocated_contribution_amount";
-    public final static String TOTAL_UNALLOCATED_CONTRIBUTION_AMOUNT = "total_unallocated_consumption_amount";
+    public final static String TOTAL_UNALLOCATED_CONSUMPTION_AMOUNT = "total_unallocated_consumption_amount";
+    public final static String TOTAL_UNALLOCATED_CONTRIBUTION_AMOUNT = "total_unallocated_contribution_amount";
     public final static String GET_CURRENT_BALANCE = "select * from %s where %s = ? order by %s desc limit 1";
     public final static String GET_BALANCE_FOR_DATE = "select * from %s where %s = ? and CAST(%s as DATE) = ? order by %s desc limit 1";
     
