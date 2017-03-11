11e0da2f88efb26cd7020d5b2ce5076581628f1a
KostyantynPanchenko <kostyantyn.panchenko@gmail.com>
2017-03-11 13:13:41 +0200

    Added balance calculation stored procedure

---
+++ api/src/main/resources/changelog/sample_data.sql
@@ -0,0 +1,22 @@
+INSERT INTO stores(tenant_id, name, description) VALUES(23, 'MJ', 'Michael Jordan');
+INSERT INTO stores(tenant_id, name, description) VALUES(33, 'PIP', 'Scotie Pippen');
+INSERT INTO stores(tenant_id, name, description) VALUES(34, 'Shaq', 'Shaquille Oneil');
+
+INSERT INTO registers(store_id, name, timezone) VALUES(1, 'MJ STORE No1', null);
+INSERT INTO registers(store_id, name, timezone) VALUES(1, 'MJ STORE No2', null);
+INSERT INTO registers(store_id, name, timezone) VALUES(1, 'MJ STORE No3', null);
+INSERT INTO registers(store_id, name, timezone) VALUES(2, 'Pippen Store No1', null);
+INSERT INTO registers(store_id, name, timezone) VALUES(2, 'Pippen Store No2', null);
+INSERT INTO registers(store_id, name, timezone) VALUES(2, 'Pippen Store No3', null);
+INSERT INTO registers(store_id, name, timezone) VALUES(2, 'Pippen Store No4', null);
+INSERT INTO registers(store_id, name, timezone) VALUES(2, 'Pippen Store No5', null);
+INSERT INTO registers(store_id, name, timezone) VALUES(3, 'Shaq No1', null);
+INSERT INTO registers(store_id, name, timezone) VALUES(3, 'Shaq No1', null);
+INSERT INTO registers(store_id, name, timezone) VALUES(3, 'Shaq No1', null);
+
+INSERT INTO contribution_transactions(register_id, order_granted_value, created_on, created_by) VALUES(1, 30, now(), 'Jonh Doe');
+INSERT INTO contribution_transactions(register_id, order_granted_value, created_on, created_by) VALUES(1, 50, now() + interval '2 days', 'Jonh Doe');
+
+INSERT INTO consumption_transactions(register_id, consumed_value, created_on, created_by) VALUES(1, 15, now() + interval '1 day', 'Jane Doe');
+INSERT INTO consumption_transactions(register_id, consumed_value, created_on, created_by) VALUES(1, 40, now() + interval '3 days', 'Jane Doe');
+INSERT INTO consumption_transactions(register_id, consumed_value, created_on, created_by) VALUES(1, 25, now() + interval '5 days', 'Jane Doe');
